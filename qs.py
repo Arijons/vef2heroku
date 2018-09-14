fcdc97670207088339affa0788d9905cfcf178c3
Arijons <33861569+Arijons@users.noreply.github.com>
2018-09-14 18:28:08 +0000

    Update qs.py

--- qs.py
+++ qs.py
@@ -10,5 +10,11 @@ def index():
 def server_static(filename):
     return static_file(filename, root="./")
 
+@route('/forum')
+def display_forum():
+    forum_id = request.query.id
+    forum_page = request.query.page
+    return "id: " + forum_id + "<br>" + "page: " + forum_page
+
 if  __name__=='__main__':
-    run(reloader=True, debug=True)
+    run()
