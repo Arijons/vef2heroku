e7451a53f72236ad19ababdf22f978f35f5bfc8b
Arijons <33861569+Arijons@users.noreply.github.com>
2018-09-14 18:49:20 +0000

    Add files via upload

---
+++ Verkefni2B/qs.py
@@ -0,0 +1,20 @@
+
+from bottle import run, route, template, static_file, request
+
+@route('/')
+def index():
+    return template('index')
+
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root="./")
+
+@route('/forum')
+def display_forum():
+    forum_id = request.query.id
+    forum_page = request.query.page
+    return "id: " + forum_id + "<br>" + "page: " + forum_page
+
+if  __name__=='__main__':
+    run()
